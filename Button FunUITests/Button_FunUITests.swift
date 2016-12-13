d5e635ee9cf5f8bf53cd8e88d40384a70a67057e
Nicholas Tran <2010435@crpwit1467smini.laclubs.com>
2016-12-13 08:25:49 -0800

    Added test steps

--- Button FunUITests/Button_FunUITests.swift
+++ Button FunUITests/Button_FunUITests.swift
@@ -31,6 +31,11 @@ class Button_FunUITests: XCTestCase {
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let app = XCUIApplication()
+        app.buttons["Left"].tap()
+        app.buttons["Right"].tap()
+        
     }
     
 }
